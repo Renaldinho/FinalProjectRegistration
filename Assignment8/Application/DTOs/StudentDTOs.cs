e2f8bd4fd0cfd0368afa37a0e21f395991e1ff6d
Renaldinho <rena0968@easv365.dk>
2022-11-02 11:13:56 +0100

    api

---
+++ Assignment8/Application/DTOs/StudentDTOs.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.DTOs;
+
+public class PostStudentDTO
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public int ZipCode { get; set; }
+    public string PostalDistrict { get; set; }
+    public string Email { get; set; }
+}
---
+++ Assignment8/Infrastructure/StudentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class StudentDbContext: DbContext
+{
+    public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+}
